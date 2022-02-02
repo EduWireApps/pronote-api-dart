0b4618668720bfa6d6c4c8e9ae2b699f47d90b47
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-02-02 21:51:32 +0100

    finish KeepAlive

--- lib/src/communication.dart
+++ lib/src/communication.dart
@@ -5,10 +5,15 @@ class Communication {
   late final String urlRoot;
   late final String urlPath;
   late Encryption encryption;
+  late double lastPing;
 
   Communication(this.url) {
     final split = Utils.splitAdress(url);
     urlRoot = split[0];
     urlPath = split[1];
+  }
+
+  Future<Response<Map<String, dynamic>>> post(String name, {dynamic data}) async {
+    return Response(error: "Not implemented");
   }
 }
--- lib/src/keep_alive.dart
+++ lib/src/keep_alive.dart
@@ -1,1 +1,21 @@
 part of pronote_api_dart;
+
+class KeepAlive {
+  late Communication communication;
+  bool keepAlive = true;
+
+  KeepAlive(PronoteClient client) {
+    communication = client.communication;
+  }
+
+  Future<void> alive() async {
+    while (keepAlive) {
+      if (DateTime.now().millisecondsSinceEpoch / 1000 - communication.lastPing >= 300) {
+        communication.post("Presence", data: {
+          '_Signature_': {'onglet': 7}
+        });
+      }
+      await Future.delayed(const Duration(seconds: 1));
+    }
+  }
+}
